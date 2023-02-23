bcbb1bf960d262f29ac4952f21e16dfcfc1adbe1
shatudas <das.shatu2000@gmail.com>
2023-02-23 10:11:01 +0600

    Read PHP Introduction

---
+++ PHP Introduction.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP Introduction</title>
+</head>
+<body>
+
+
+	<?php
+	echo "
+	<table>
+		<tr>
+			<td>
+				<h2>What is PHP?</h2>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<span>PHP is an acronym for PHP: Hypertext Preprocessor</span><br>
+				<span>PHP is a widely-used, open source scripting language</span><br>
+				<span>PHP scripts are executed on the server</span><br>
+				<span>PHP is free to download and use</span>
+			</td>
+		</tr>
+
+		<tr>
+   <td><h2>Why PHP?</h2></td>
+		</tr>
+		<tr>
+		 <td>
+    <span>PHP runs on various platforms (Windows, Linux, Unix, Mac OS X, etc.)<span><br>
+    <span>PHP is compatible with almost all servers used today (Apache, IIS, etc.)<span><br>
+    <span>PHP supports a wide range of databases<span><br>
+    <span>PHP is free. Download it from the official PHP<span><br>
+    <span>PHP is easy to learn and runs efficiently on the server side<span><br>
+   <td>
+		</tr>
+
+		<tr>
+   <td><h2>PHP files have extension .php</h2></td>
+		</tr>
+
+
+
+		<tr>
+    <td><h2>What Can PHP Do?</h2></td>
+		</tr>
+
+		<tr>
+		 <td>
+	   <span>PHP can generate dynamic page content<span><br>
+	   <span>PHP can create, open, read, write, delete, and close files on the server<span><br>
+	   <span>PHP can collect form data<span><br>
+	   <span>PHP can send and receive cookies<span><br>
+	   <span>PHP can add, delete, modify data in your database<span><br>
+	   <span>PHP can be used to control user-access<span><br>
+	   <span>PHP can encrypt data<span><br>
+	   <span>With PHP you are not limited to output HTML. You can output images or PDF files. You can also output any text, such as XHTML and XML.<span><br>
+   <td>
+		</tr>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	</table>";
+
+	?>
+
+
+	
+
+</body>
+</html>
---
+++ test print.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+	<h2>php practices</h2>
+
+	<?php
+	echo "Hello PHP language <br>";
+
+	$a="Hello PHP language <br>";
+	print $a;
+
+	$value1 = 10;
+	$value2 = 30;
+	$value3 = ($value1 + $value2 );
+	print($value3); 
+
+
+
+	?>
+
+</body>
+</html>
