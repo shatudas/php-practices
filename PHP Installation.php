f3d8d9f3a65879a4bec20bc0bb51231f94b18c1b
shatudas <das.shatu2000@gmail.com>
2023-02-23 19:58:18 +0600

    php introduction

---
+++ PHP Installation.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP Installation</title>
+</head>
+<body>
+
+
+	<?php
+	echo "
+	<table>
+		
+		<tr>
+			<td>
+				<h1>your  PHP using What Do Need?</h1>
+			</td>
+		</tr>
+	
+		<tr>
+			<td>
+				<span>Find a web host with PHP and MySQL support Install a web server on your own PC, and then install PHP and MySQL</span><br>
+			</td>
+		</tr>
+
+		<tr>
+    <td><h2>Use a Web Host With PHP Support</h2></td>
+		</tr>
+
+		<tr>
+		 <td><span>If your server has activated support for PHP you do not need to do anything.<span><br><td>
+		</tr>
+
+		<tr>
+    <td><h2>Set Up PHP on Your Own PC</h2></td>
+		</tr>
+
+	 <tr>
+		 <td>
+	   <span>install a web server<span><br>
+	   <span>install PHPr<span><br>
+	   <span>install a database, such as MySQL<span><br><br><br>
+	   <span>**Just create some .php files, place them in your web directory, and the server will automatically parse them for you.<span><br> 
+   <td>
+		</tr>
+
+	</table>";
+
+	?>
+
+
+	
+
+</body>
+</html>
