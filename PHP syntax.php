2f7ea765a5f449d924626f906701636ca575ebfc
shatudas <das.shatu2000@gmail.com>
2023-02-23 20:23:11 +0600

    php syntex

---
+++ PHP syntax.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP Installation</title>
+</head>
+<body>
+
+<h2> Php  syntax example</h2><br><br>
+	<?php
+  echo "Hello World!<br>";
+ ?>
+  <span>*** Note: PHP statements end with a semicolon (;).</span><br>
+
+  <h2>PHP Case Sensitivity</h2><br>
+  <span>In PHP, keywords (e.g. if, else, while, echo, etc.), classes, functions, and user-defined functions are not case-sensitive.</span><br><br><br>
+   <h2> Php Case Sensitivity</h2><br>
+  <?php
+			ECHO "Hello World!<br>";
+			echo "Hello World!<br>";
+			EcHo "Hello World!<br><br>";
+			?>
+
+			<span>Note: However; all variable names are case-sensitive!</span>
+	
+
+</body>
+</html>
