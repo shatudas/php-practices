93262c765768ad7490b5ccada9658e60432d08e1
shatudas <das.shatu2000@gmail.com>
2023-02-23 23:07:42 +0600

    php data type

---
+++ PHPDataType.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP data type</title>
+</head>
+<body>
+
+<h2> Php  Data Type List</h2><br><br>
+<ul>
+	<li>String</li>
+	<li>Integer</li>
+	<li>Float</li>
+	<li>Boolean</li>
+	<li>Array</li>
+	<li>Object</li>
+	<li>NULL</li>
+	<li>Resource</li>
+</ul>
+
+<h2>Example  String Data Type</h2><br>
+
+<?php
+$x = "String Data Type";
+echo $x;
+?>
+
+<h2>Example  String Data Type</h2><br>
+
+<?php
+$int= 500;
+var_dump($int);
+?>
+
+<h2>Example  floot Data Type</h2><br>
+
+<?php
+ $floot= 50.50;
+ var_dump($floot);
+?>
+
+<h2>Example  array Data Type</h2><br>
+
+<?php
+$vararray= array('valueone','valuetwo','valuethree');
+var_dump($vararray);
+
+?>
+
+
+<h2>Example Null Type</h2><br>
+
+<?php
+ $varnull=Null;
+ var_dump($varnull);
+?>
+
+
+
+</body>
+</html>
