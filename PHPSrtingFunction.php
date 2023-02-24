a85dab67433765078c01333177fd7d2cb538387b
shatudas <das.shatu2000@gmail.com>
2023-02-24 17:50:09 +0600

    php srting funcation

---
+++ PHPSrtingFunction.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP Srting Funcation</title>
+</head>
+<body>
+
+<h2>php srting strlen() Funcation</h2><br>
+<span>The PHP strlen() function returns the length of a string.</span><br>
+
+<h2>Example strlen() funcation For Hello PHP </h2><br>
+
+<?php
+  echo strlen("Hello PHP");
+?>
+
+
+<h2>php srting str_word_count() Funcation</h2><br>
+<span>The PHP str_word_count() function counts the number of words in a string.</span><br>
+
+<h2>Example str_word_count() funcation For Hello PHP </h2><br>
+
+<?php
+  echo str_word_count("Hello PHP");
+?>
+
+
+<h2>php srting strrev() Funcation</h2><br>
+<span>The PHP strrev() function reverses a string.</span><br>
+
+<h2>Example strrev() funcation For Hello PHP </h2><br>
+
+<?php
+  echo strrev("Hello PHP");
+?>
+
+<h2>php srting strpos() Funcation</h2><br>
+<span>The PHP strpos() function searches for a specific text within a string. If a match is found, the function returns the character position of the first match. If no match is found, it will return FALSE.</span><br>
+
+<h2>Example strpos() funcation For Hello PHP </h2><br>
+
+<?php
+  echo strpos("Hello PHP", "PHP"); 
+?>
+
+
+<h2>php srting str_replace() Funcation</h2><br>
+<span>he PHP str_replace() function replaces some characters with some other characters in a string.</span><br>
+
+<?php
+echo str_replace('Hello','Computer', 'Hello PHP')
+?>
+<br><br><br><br>
+
+
+</body>
+</html>
